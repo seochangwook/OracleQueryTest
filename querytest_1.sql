d48d44cac13d0633aa0a92d6eb8d181d01284823
seochangwook <scw05313315@gmail.com>
2017-09-19 18:05:03 +0900

    querytest_1 fix-1

--- querytest_1.sql
+++ querytest_1.sql
@@ -1,1 +1,18 @@
+/* 유저 테이블 생성 및 검증 */
+create table userinfo
+(
+    user_id varchar2(50), --유저 아이디
+    user_password varchar2(150), -- 유저 비밀번호
+    user_email varchar2(150), -- 유저 이메일
+    user_phonenumber varchar2(50), -- 유저 핸드폰 번호
+    user_manage number(2), --관리자 유무
+    user_reg_date DATE, -- 가입날짜
+    user_push_use number(2), -- 이메일 알림 유무
+    primary key(user_id)
+);
+
 COMMIT;
+    
+select * from userinfo;
+
+/* 유저 추가 */
